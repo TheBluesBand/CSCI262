37472ac8d18a8f3df6bd09e568e18fe93177c552
Jake McCoy <the.mccoy@outlook.com>
2024-08-22 14:06:50 +1000

    Current progress on lab does not work

--- Lab5/lab.py
+++ Lab5/lab.py
@@ -1,25 +1,38 @@
 import hashlib
+import os
+
 flag = 0
 # paste the tested hashed code in the double quotation
-real_hash = ""
+real_hash = "0144e99a8cd56304aff370c875cbecb5"
 try:
-    guessed_pass = open("guessed_pass.txt","r")
-except:
+    file_path = os.path.join(os.path.dirname(__file__), 'guessed_pass.txt')
+    guessed_pass = open(file_path ,"r")
+
+    real_hashes_path = os.path.join(os.path.dirname(__file__), 'real_hashed.txt')
+    real_hashes = open(real_hashes_path ,"r")
+
+    for g_pass in guessed_pass:
+        guessed_hash = hashlib.md5(g_pass.encode('utf8').strip()).hexdigest()
+        for line in real_hashes:
+            line = line.strip()
+            print(guessed_hash == line)
+            print(guessed_hash)
+            print(line)
+            print()
+            if guessed_hash == line:
+                print("Congratulations!")
+                print("The real password of md5 hash is found" + line)
+                print("It is "+ g_pass)
+                # flag to 1 if password we found in the list
+                flag = 1
+                break
+    # if no match from the file the flag still 0
+    if flag == 0:
+        # password is not in the list
+        print("The plain password is not found in the guessed password file")
+
+except e:
     print("Guessed password file is not found")
-
-
-for g_pass in guessed_pass:
-    guessed_hash = hashlib.md5(g_pass.encode('utf8').strip()).hexdigest()
-    if guessed_hash == real_hash:
-        print("Congratulations!")
-        print("The real password of md5 hash is found" + real_hash)
-        print("It is "+ g_pass)
-        # flag to 1 if password we found in the list
-        flag = 1
-        break
-# if no match from the file the flag still 0
-if flag == 0:
-    # password is not in the list
-    print("The plain password is not found in the guessed password file")
+    print(e)
 
 quit()
